e0f90c8b24ecfc0bd227c5581d6bb8e786cd7ac5
Onel Medina <43082323+onelmv@users.noreply.github.com>
2021-10-25 10:49:43 -0400

    trailing whitespaces and general formating fixit (by pylint)

--- IPg.py
+++ IPg.py
@@ -8,26 +8,27 @@ IPg receives two values as arguments: the file and the API key
 IPg.py <file path> <API key>
 
 """
+import os
 import sys
 import requests
-import os
+
 
 
 # global variable. modifiable depending on the data you want to obtain
-API_DATA = ["ip","country_name","state_prov","city","isp",] 
-API_URL = "https://api.ipgeolocation.io/ipgeo" 
+API_DATA = ["ip","country_name","state_prov","city","isp",]
+API_URL = "https://api.ipgeolocation.io/ipgeo"
 
 def geolocate_cidr(ip, key):
-    #storing the ip without the CIDR 
+    #storing the ip without the CIDR
     ip = ip.split('/')
-        
+
     data = requests.get(f"{API_URL}?apiKey={key}&ip={ip[0]}").json()
     output(data)
 
 
 def geolocate_file(ip_list, key):
      #request information from a list of IP addresses obtained from a text file
-        
+
     for ip in ip_list:
         if '/' in ip:
             # if the IP addres is in CIDR format
@@ -40,28 +41,28 @@ def geolocate_file(ip_list, key):
             output(data)
 
 def geolocate_ip(ip, key):
-    
+
     if '/' in ip:
-            # if the IP addres is in CIDR format
-            geolocate_cidr(ip, key)
+        # if the IP addres is in CIDR format
+        geolocate_cidr(ip, key)
     else:
         #request information of a specific IP provided by the user.
         data = requests.get(f"{API_URL}?apiKey={key}&ip={ip}").json()
-        
+
         output(data)
 
 def output(data):
-    """Print the data 
-       output : ip, country_name, state_prov, city, isp    
+    """Print the data
+       output : ip, country_name, state_prov, city, isp
     """
     print("================================")
-    
-    # message in case the ip address are not correct. 
+
+    # message in case the ip address are not correct.
     if len(data) == 1:
         print("[!] IP NOT FOUND")
         print("[!] check IP addresses")
         print("[!] check your key")
-    
+
     i = 0
     for elem in data:
         if elem == API_DATA[i]:
@@ -72,24 +73,21 @@ def output(data):
 def main(argv):
     # check if it's a file with the IP addresses or a typed ip
     if os.path.isfile(argv[1]):
-        # this array will contain all the ip addresses 
+        # this array will contain all the ip addresses
         ip_list = []
-        
+
         with open(argv[1], 'r') as iFile:
             for ip in iFile:
                 ip_list.append(ip)
         geolocate_file(ip_list, argv[2])
-    
+
     else:
         geolocate_ip(argv[1], argv[2])
 
-
 if __name__ == "__main__":
-    # making sure the 2 arguments is present. 
-    if len(sys.argv) == 3:        
+    # making sure the 2 arguments is present.
+    if len(sys.argv) == 3:
         main(sys.argv)
-    else:    
+    else:
         print("Format : IPg.py <file path> <API key>")
         print("Format : IPg.py <IP address> <API key>")
-    
-   
